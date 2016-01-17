8d78338bb5f23d0e33ca627ee8fa5f0921edb6c2
Bodie Solomon <binary132@gmail.com>
2016-01-17 08:03:48 -0500

    Initial commit

---
+++ phoenix/phoenix.go
@@ -0,0 +1,8 @@
+// Phoenix: an application generator for Go.
+// Copyright (C) 2016 Bodie Solomon
+// Licensed under the GNU Affero General Public License version 3.  See LICENSE.
+
+package main
+
+func main() {
+}
